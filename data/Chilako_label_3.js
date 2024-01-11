a6c171e2aad7e6e3132d206c88ba9f56548f1ada
lmatechuk <landenmatechuk@gmail.com>
2024-01-11 12:21:25 -0800

    Initial commit

---
+++ data/Chilako_label_3.js
@@ -0,0 +1,8 @@
+var json_Chilako_label_3 = {
+"type": "FeatureCollection",
+"name": "Chilako_label_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Name": "Chilako River Watershed" }, "geometry": { "type": "Point", "coordinates": [ -123.587281665357139, 53.555574613879052 ] } }
+]
+}
